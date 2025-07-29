782b2cfb3464e8656167a16c4125bb478fdc0df5
martin.aguado <martin.aguado@bosonit.com>
2025-07-29 13:41:57 +0200

    feat(app): classic deploy & import some angular/material stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,34 @@
-import { HttpClientModule } from '@angular/common/http';
+// src/app/app.module.ts
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
+import { AppComponent } from './app.component';
+import { CandidateFormComponent } from './features/candidate/components/candidate-form/candidate-form.component';
 @NgModule({
-    imports: [
-        HttpClientModule,
+    declarations: [
+        AppComponent,
+        CandidateFormComponent,
     ],
-    declarations: [],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatTableModule,
+        MatToolbarModule,
+        MatCardModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
