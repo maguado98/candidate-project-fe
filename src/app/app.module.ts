99d012e7367d278490b9f97c1eb9fe8823189344
martin.aguado <martin.aguado@bosonit.com>
2025-07-29 12:48:20 +0200

    feat(app-module): add AppModule file

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [
+        HttpClientModule,
+    ],
+    declarations: [],
+})
+export class AppModule { }
