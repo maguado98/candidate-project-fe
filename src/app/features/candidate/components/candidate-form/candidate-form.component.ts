e74e4bf4afa4d30bfaa22a103a6f4257a93413c0
martin.aguado <martin.aguado@bosonit.com>
2025-07-29 12:52:24 +0200

    feat(candidate-form): create candidate-form

---
+++ src/app/features/candidate/components/candidate-form/candidate-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-candidate-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './candidate-form.component.html',
+  styleUrls: ['./candidate-form.component.scss']
+})
+export class CandidateFormComponent {
+
+}
